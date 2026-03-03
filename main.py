050e1ea1fcbe2db581f4e85ed224c2313e653379
Hugo Martin De la Puente Solorzano <HDelaPuente@tramarsa.com.pe>
2026-03-03 11:38:03 -0500

    Configuración CORS para integración con React

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from routes.empresas import router as empresas_router
 from routes.obligaciones import router as obligaciones_router
+from routes.auth import router as auth_router
 
 app = FastAPI(title="LexControl RH API")
 
+# 🔥 CONFIGURACIÓN CORS
+origins = [
+    "http://localhost:5173",  # React en desarrollo
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(empresas_router)
 app.include_router(obligaciones_router)
+app.include_router(auth_router)
 
 @app.get("/")
 def home():
     return {"mensaje": "LexControl RH API funcionando 🚀"}
-
-from routes.auth import router as auth_router
-
-app.include_router(auth_router)
